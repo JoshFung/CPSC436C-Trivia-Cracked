6f4d2ce3ce23e501069357e9573be8f7e67d1b74
Josh Fung <joshuagyfung@gmail.com>
2024-11-25 16:54:30 -0800

    Added colour for 'other' category

--- src/ts/enums/CategoryColourEnum.ts
+++ src/ts/enums/CategoryColourEnum.ts
@@ -2,9 +2,10 @@ enum CategoryColourEnum {
   "Art" = "#e7c6ff",
   "Entertainment" = "#ffb3c1",
   "Geography" = "#c7f9cc",
-  "History" = "#eae0d5",
+  "History" = "#E4BB97",
   "Science" = "#cbeef3",
   "Sports" = "#FFE285",
+  "Other" = "#eae0d5"
 }
 
 export default CategoryColourEnum;
